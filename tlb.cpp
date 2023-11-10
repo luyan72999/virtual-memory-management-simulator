834978ec998bc62c3dd5027adb3c3a52a10628e3
luyan72999 <113495445+luyan72999@users.noreply.github.com>
2023-11-10 14:51:41 -0800

    Change L2 TLB data structure

--- tlb.cpp
+++ tlb.cpp
@@ -5,7 +5,6 @@
 #include <random>
 #include <ctime>
 #include <cmath>
-#include <stdexcept>
 
 using namespace std;
 
@@ -30,17 +29,24 @@ class TlbEntry {
 class Tlb {
   public:
     vector<TlbEntry*>* l1_list;
-    vector<TlbEntry*>* l2_list;   
+    vector<vector<TlbEntry*>*>* l2_list;   
     uint32_t l1_size;
     uint32_t l2_size;
+    uint32_t max_process_allowed; // max number of processes that can exist in l2, default 4
+    uint32_t l2_size_per_process; // default 1024/4 = 256
 
     // constructor
-		Tlb(uint32_t l1_size, uint32_t l2_size):l1_size(l1_size),l2_size(l2_size){
-			//by default: l1 size 64, l2 size 1024 
+		Tlb(uint32_t l1_size, uint32_t l2_size, uint32_t max_process_allowed):l1_size(l1_size),l2_size(l2_size), max_process_allowed(max_process_allowed){
+			//by default: l1 size 64, l2 size 1024, max process allowed is 4
 
       l1_list = new vector<TlbEntry*>();
-      l2_list = new vector<TlbEntry*>();
-      
+      l2_list = new vector<vector<TlbEntry*>*>();
+
+      for (int i = 0; i < max_process_allowed; i++) {
+        (*l2_list)[i] = new vector<TlbEntry*>();
+      }
+
+      l2_size_per_process = l2_size / max_process_allowed;
 		}
 
     PTEntry* get_pt_entry(uint32_t virtual_addr) {
@@ -55,7 +61,7 @@ class Tlb {
       // calculate mask: number of bits to right shift to extract vpn. 
       // e.g. set mask to 12 when page size is 4KB and physical mem is 4GB (thus 20-bit vpn).
       uint32_t page_size = pt_entry->page_size;
-      uint32_t mask = 32 - pow(2,30) / page_size;
+      uint32_t mask = log2(page_size);
       uint32_t vpn = virtual_addr >> mask; 
 
       uint32_t pfn = pt_entry->pfn;
@@ -69,9 +75,9 @@ class Tlb {
     // look_up(): given a vpn, look it up in both l1 and l2
     // returns pfn if found
     // returns -1 if not found, os needs to handle this exception
-    uint32_t look_up(uint32_t virtual_addr, uint32_t page_size) {
+    uint32_t look_up(uint32_t virtual_addr, uint32_t page_size, uint32_t process_id) {
       // exact vpn based on page_size
-      uint32_t mask = 32 - pow(2,30) / page_size;
+      uint32_t mask = log2(page_size);
       uint32_t vpn = virtual_addr >> mask;
 
       // first, check l1
@@ -80,14 +86,14 @@ class Tlb {
           return (*l1_list)[i]->pfn;
         }
       }
-      // not in l1, check l2
-      for (int i = 0; i < l2_list->size(); i++) {
-        if ((*l2_list)[i]->vpn == vpn) {
-          // if found, insert this item to l1
-          l1_insert((*l2_list)[i]);
-          return (*l2_list)[i]->pfn;
-        }
-      }
+      // not in l1, check l2: TBD
+      // for (int i = 0; i < l2_list->size(); i++) {
+      //   if ((*l2_list)[i]->vpn == vpn) {
+      //     // if found, insert this item to l1
+      //     l1_insert((*l2_list)[i]);
+      //     return (*l2_list)[i]->pfn;
+      //   }
+      // }
       // how to return if not found? can return -1 but return type is unsigned int 
       // not found in both tlbs: os goes to page table and get pfn and meta data
       
@@ -95,10 +101,18 @@ class Tlb {
 
 
     // upon TLB hit, assemble physical address: if found in tlb, use pfn and offset to form a physicai address
-    uint32_t assemble_physical_addr(TlbEntry* entry, uint32_t virtual_addr) {
-      // get the offset based on page size
+    uint32_t assemble_physical_addr(TlbEntry* tlb_entry, uint32_t virtual_addr) {
+      // get the offset length based on page size
+      uint32_t page_size = tlb_entry->page_size;
+      uint32_t offset_length = log2(page_size);
+
+      // get the value of offset
+      uint32_t mask = (1 << offset_length) - 1;
+      uint32_t offset = virtual_addr & mask;
 
       // assembly pfn + offset
+      uint32_t physical_addr; // TBD
+      return physical_addr;
     }
 
 
@@ -121,7 +135,7 @@ class Tlb {
       }
     }
     
-    // maximum 256 entries allowed per process
+    // default: maximum 256 entries allowed per process
     void l2_insert(TlbEntry* entry) {
       
     }
